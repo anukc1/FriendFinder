d3b17a1d237cee37461bc62eca06e9eb114a6639
Anu KC <anukc2000@gmail.com>
2019-07-18 16:35:18 -0500

    button working better

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ var express = require("express");
 
 // Tells node that we are creating an "express" server
 var app = express();
+var bodyParser = require('body-parser');
+var path = require("path");
 
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8081;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json({type: 'application/vnd.custom-type'}));
+
+app.use(bodyParser.text({type: 'text/html'}))
 
 require("./app/routing/htmlRoutes.js")(app);
 require("./app/routing/apiRoutes.js")(app);
