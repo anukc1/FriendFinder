079b4fa6d62191bb8aedee078251a7a2fd935fc6
Anu KC <anukc2000@gmail.com>
2019-07-18 17:47:31 -0500

    form into not connecting to friends api

--- server.js
+++ server.js
@@ -4,18 +4,14 @@ var express = require("express");
 
 // Tells node that we are creating an "express" server
 var app = express();
-var bodyParser = require('body-parser');
-var path = require("path");
 
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8081;
+var path = require("path");
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(bodyParser.json({type: 'application/vnd.custom-type'}));
-
-app.use(bodyParser.text({type: 'text/html'}))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 require("./app/routing/htmlRoutes.js")(app);
 require("./app/routing/apiRoutes.js")(app);
